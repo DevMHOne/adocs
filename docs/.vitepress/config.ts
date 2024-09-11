8a27ce4894e8c9aeaf9513de662cd94006146efd
DevMHOne <110272440+DevMHOne@users.noreply.github.com>
2024-09-12 06:55:14 +0700

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -16,7 +16,7 @@ export default {
     nav: [
       { text: 'Home', link: '/' },
       { text: 'Guide', link: '/guide/introduction' },
-      { text: 'Playground', link: 'https://webcrack.netlify.app' },
+      { text: 'Playground', link: 'https://cracked.eu.org/' },
     ],
     // Social Icons
     socialLinks: [
